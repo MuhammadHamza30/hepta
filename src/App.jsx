fe3b73157467408d8862863d26ad085ad781c855
Muhammad Hamza <132424951+MuhammadHamza30@users.noreply.github.com>
2024-08-23 16:57:13 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,24 @@
-import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './Pages/Home.jsx';
-import Hotel from './Pages/Hotel.jsx';
-import AboutUs from './Pages/AboutUs.jsx';
-import Gallery from './Pages/Gallery.jsx';
-import News from './Pages/News.jsx';
-import Contact from './Pages/Contact.jsx';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/hotel",
-    element: <Hotel />,
-  },
-  {
-    path: "/AboutUs",
-    element: <AboutUs />,
-  },
-  {
-    path: "/Gallery",
-    element: <Gallery />,
-  },
-  {
-    path: "/News",
-    element: <News />,
-  },
-  {
-    path: "/Contact",
-    element:<Contact/>
-  }
-]);
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Hotel from './components/Hotel';
+import Gallery from './components/Gallery';
+import AboutUs from './components/AboutUs';
+import News from './components/News';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/hotel" component={Hotel} />
+        <Route path="/Gallery" component={Gallery} />
+        <Route path="/AboutUs" component={AboutUs} />
+        <Route path="/News" component={News} />
+        <Route path="/Contact" component={Contact} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
